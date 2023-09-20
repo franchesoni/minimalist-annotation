9de0679f7102eb99656aca2ef474cc8651df5c3e
babouslt <bastienselot@gmail.com>
2023-09-20 16:40:45 +0200

    loading photos

--- app.js
+++ app.js
@@ -1,110 +1,166 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', () => {
     const canvasContainer = document.getElementById('canvas-container');
     const canvas = document.getElementById('zoomable-canvas');
-    const coordinatesElement = document.getElementById('coordinates');
+    const uploadForm = document.getElementById('uploadForm');
+    const imageInput = document.getElementById('imageInput');
+    const coordinates = document.getElementById('coordinates');
+    const mouseCoordinates = document.getElementById('mouse-coordinates');
     const ctx = canvas.getContext('2d');
-    const image = new Image();
-    image.src = 'images/paysage.jpg'; // Remplacez par le chemin de votre image
 
+    let image = new Image();
     let scale = 1;
+    let maxScale = 0;
     let offsetX = 0;
     let offsetY = 0;
-    let isDragging = false;
-    let startX, startY;
+    let mouseDown = false;
+    let lastMouseX = 0;
+    let lastMouseY = 0;
 
-    function updateCoordinates() {
-        const scaledWidth = image.width * scale;
-        const scaledHeight = image.height * scale;
-    
-        // Calculez les coordonnées du coin supérieur gauche de la zone visible
-        const topLeftX = (-offsetX / scale);
-        const topLeftY = (-offsetY / scale);
-    
-        // Calculez les coordonnées du coin inférieur droit de la zone visible
-        const bottomRightX = topLeftX + (canvas.width / scale);
-        const bottomRightY = topLeftY + (canvas.height / scale);
-    
-        coordinatesElement.textContent = `Coordonnées : (${topLeftX.toFixed(2)}, ${topLeftY.toFixed(2)}) - (${bottomRightX.toFixed(2)}, ${bottomRightY.toFixed(2)})`;
+    function drawImage() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(image, offsetX, offsetY, image.width * scale, image.height * scale);
+
+        // Calculate the coordinates of the top-left point
+        const topLeftX = -offsetX / scale;
+        const topLeftY = -offsetY / scale;
+
+        // Calculate the coordinates of the bottom-right point
+        const bottomRightX = topLeftX + canvas.width / scale;
+        const bottomRightY = topLeftY + canvas.height / scale;
+
+        // Update the displayed coordinates
+        coordinates.textContent = `Top Left: (${topLeftX.toFixed(2)}, ${topLeftY.toFixed(2)}) | Bottom Right: (${bottomRightX.toFixed(2)}, ${bottomRightY.toFixed(2)})`;
     }
 
-    image.onload = () => {
-        // Dessinez l'image d'origine sur le canvas
-        ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+    function zoomToMouse(mouseX, mouseY, delta) {
+        const newScale = scale - delta;
+        const mouseCanvasX = (mouseX - offsetX) / scale;
+        const mouseCanvasY = (mouseY - offsetY) / scale;
+        const newOffsetX = mouseX - mouseCanvasX * newScale;
+        const newOffsetY = mouseY - mouseCanvasY * newScale;
 
-        canvasContainer.addEventListener('wheel', (event) => {
-            event.preventDefault();
+        if (newScale < maxScale) {
+            return;
+        }
 
-            scale += event.deltaY * -0.01;
-            scale = Math.min(Math.max(0.5, scale), 10000); // Limiter l'échelle entre 0.5x et 3x
+        scale = newScale;
+        offsetX = newOffsetX;
+        offsetY = newOffsetY;
 
-            // Effacez le canvas
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        const minX = Math.min(0, canvas.width - image.width * scale);
+        const minY = Math.min(0, canvas.height - image.height * scale);
+        offsetX = Math.max(minX, Math.min(offsetX, 0));
+        offsetY = Math.max(minY, Math.min(offsetY, 0));
 
-            // Redessinez l'image avec l'échelle actuelle
-            const scaledWidth = image.width * scale;
-            const scaledHeight = image.height * scale;
+        drawImage();
+    }
 
-            // Mettez à jour les offsets pour conserver la position pendant le zoom
-            offsetX = Math.min(Math.max(offsetX, canvas.width - scaledWidth), 0);
-            offsetY = Math.min(Math.max(offsetY, canvas.height - scaledHeight), 0);
+    
 
-            ctx.drawImage(image, offsetX, offsetY, scaledWidth, scaledHeight);
+    uploadForm.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-            updateCoordinates();
-        });
+        // Get the image file selected by the user
+        const selectedImage = imageInput.files[0];
 
-        canvasContainer.addEventListener('mousedown', (event) => {
-            isDragging = true;
-            startX = event.clientX - canvasContainer.getBoundingClientRect().left;
-            startY = event.clientY - canvasContainer.getBoundingClientRect().top;
-        });
+        if (!selectedImage) {
+            alert('No image selected.');
+            return;
+        }
 
-        canvasContainer.addEventListener('mousemove', (event) => {
-            if (isDragging) {
-                const currentX = event.clientX - canvasContainer.getBoundingClientRect().left;
-                const currentY = event.clientY - canvasContainer.getBoundingClientRect().top;
-                const dx = currentX - startX;
-                const dy = currentY - startY;
-                offsetX += dx;
-                offsetY += dy;
+        // Load the selected image into the canvas
+        const reader = new FileReader();
+        reader.onload = (event) => {
+            image.src = event.target.result;
 
-                // Effacez le canvas
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
+            image.onload = () => {
+                // Calculate the maximum dimensions for the canvas
+                const maxWidth = canvasContainer.clientWidth;
+                const maxHeight = canvasContainer.clientHeight;
 
-                // Redessinez l'image avec l'échelle actuelle et les nouveaux offsets
-                const scaledWidth = image.width * scale;
-                const scaledHeight = image.height * scale;
-                offsetX = Math.min(Math.max(offsetX, canvas.width - scaledWidth), 0);
-                offsetY = Math.min(Math.max(offsetY, canvas.height - scaledHeight), 0);
-                ctx.drawImage(image, offsetX, offsetY, scaledWidth, scaledHeight);
+                // Calculate the initial maximum scale
+                scale = Math.min(maxWidth / image.width, maxHeight / image.height);
+                maxScale = scale;
 
-                startX = currentX;
-                startY = currentY;
+                // Reset the offset parameters
+                offsetX = 0;
+                offsetY = 0;
 
-                updateCoordinates();
-            }
-        });
+                // Apply the dimensions to the canvas without changing the scale
+                canvas.width = maxWidth;
+                canvas.height = maxHeight;
 
-        canvasContainer.addEventListener('mouseup', () => {
-            isDragging = false;
-        });
+                // Draw the image at the correct scale
+                drawImage();
+            };
+        };
 
-        canvasContainer.addEventListener('mouseleave', () => {
-            isDragging = false;
-        });
+        reader.readAsDataURL(selectedImage);
+    });
 
-        canvas.addEventListener('click', (event) => {
-            // Obtenez les coordonnées du clic par rapport au canvas
-            const clickX = event.clientX - canvas.getBoundingClientRect().left;
-            const clickY = event.clientY - canvas.getBoundingClientRect().top;
+    canvas.addEventListener('mousedown', (e) => {
+        mouseDown = true;
+        lastMouseX = e.clientX;
+        lastMouseY = e.clientY;
+    });
 
-            // Calculez les coordonnées réelles dans l'image, en tenant compte de l'échelle et des offsets
-            const imageX = (clickX - offsetX) / scale;
-            const imageY = (clickY - offsetY) / scale;
+    canvas.addEventListener('mouseup', () => {
+        mouseDown = false;
+    });
 
-            console.log(`Clic à (${imageX}, ${imageY})`);
-        });
-    };
+    canvas.addEventListener('mousemove', (e) => {
+        if (mouseDown) {
+            const mouseX = e.clientX;
+            const mouseY = e.clientY;
+
+            const deltaX = (mouseX - lastMouseX) / scale;
+            const deltaY = (mouseY - lastMouseY) / scale;
+
+            offsetX += deltaX;
+            offsetY += deltaY;
+
+            const minX = Math.min(0, canvas.width - image.width * scale);
+            const minY = Math.min(0, canvas.height - image.height * scale);
+            offsetX = Math.max(minX, Math.min(offsetX, 0));
+            offsetY = Math.max(minY, Math.min(offsetY, 0));
+
+            drawImage();
+
+            lastMouseX = mouseX;
+            lastMouseY = mouseY;
+        }
+        // Get the coordinates of the mouse pointer relative to the canvas
+        const mouseCanvasX = e.clientX - canvas.getBoundingClientRect().left;
+        const mouseCanvasY = e.clientY - canvas.getBoundingClientRect().top;
+        
+        // Calculate the real coordinates in the image, taking into account the scale and offsets
+        const imageX = (mouseCanvasX - offsetX) / scale;
+        const imageY = (mouseCanvasY - offsetY) / scale;
+        
+        // Display the coordinates in the console in real-time
+        mouseCoordinates.textContent = `Mouse: (${imageX.toFixed(2)}, ${imageY.toFixed(2)})`;
+    });
+
+    canvas.addEventListener('wheel', (e) => {
+        const zoomSpeed = 0.001; // Adjust the zoom speed as needed
+        const zoomDelta = e.deltaY * zoomSpeed;
+
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+
+        zoomToMouse(mouseX, mouseY, zoomDelta);
+    });
+
+    // Add click event listener to display coordinates in the console
+    canvas.addEventListener('click', (event) => {
+        // Obtenez les coordonnées du clic par rapport au canvas
+        const clickX = event.clientX - canvas.getBoundingClientRect().left;
+        const clickY = event.clientY - canvas.getBoundingClientRect().top;
+
+        // Calculez les coordonnées réelles dans l'image, en tenant compte de l'échelle et des offsets
+        const imageX = (clickX - offsetX) / scale;
+        const imageY = (clickY - offsetY) / scale;
+
+        console.log(`Clic à (${imageX.toFixed(2)}, ${imageY.toFixed(2)})`);
+    });
 });
